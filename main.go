a7778e94255957ca69204d5e1e92c275daa35b6a
Alireza Alizadeh Aghdam <78053550+AlirezaRubik@users.noreply.github.com>
2024-04-01 23:20:16 +0330

    Update main.go

--- main.go
+++ main.go
@@ -570,3 +570,28 @@ func sumOfTheDigitsOfHarshadNumber(x int) int {
 	}
 	return sum
 }
+// ////////////////////////////////////////////////////////////////
+//Find the Sum of Encrypted Integers
+func sumOfEncryptedInt(nums []int) int {
+    sum := 0
+    for _, num := range nums {
+        sum += encrypt(num)
+    }
+    return sum
+}
+func encrypt(num int) int {
+    count, large := 0, 0
+    for num > 0 {
+        digit := num % 10
+        if digit > large {
+            large = digit
+        }
+        num /= 10
+        count++
+    }
+    for i:=1; i<count; i++ {
+        large = (large * 10) + (large % 10)
+    }
+    return large
+}
+// ////////////////////////////////////////////////////////////////
